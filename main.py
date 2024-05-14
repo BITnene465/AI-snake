1afc5922b52a4ed511c27861486858b59f06b4cd
Tan Jingyuan <2073335023@qq.com>
2024-05-15 00:40:48 +0800

    命令行参数系统构建
    main

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+import argparse
 import snake
-import sys
+
+def main():
+    parser = argparse.ArgumentParser(description='Snake game with command line arguments')
+    parser.add_argument('-l', '--left', type=int, default=-4, help='left boundary')
+    parser.add_argument('-r', '--right', type=int, default=4, help='right boundary')
+    parser.add_argument('-t', '--top', type=int, default=4, help='top boundary')
+    parser.add_argument('-b', '--bottom', type=int, default=-4, help='bottom boundary')
+    parser.add_argument("--size", type=int, default=40, help='length of square')
+    parser.add_argument("--rate", type=int, default=60, help='fresh rate')
+    parser.add_argument("--score", type=float, default=11/12, help="rating of score when winning")
+
+    args = parser.parse_args()
+    snake.start(args.left, args.right, args.top, args.bottom, args.size, args.score, args.rate)
 
 if __name__ == '__main__':
-    args = sys.argv
-    count = len(args)
-    if count == 5:
-        snake.start(eval(args[1]), eval(args[2]), eval(args[3]), eval(args[4]))
-    elif count == 1:
-        snake.start()
-    else:
-        print("请输入 4个或 0个参数")
+    main()
+
